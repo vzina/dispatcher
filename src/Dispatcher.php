e41a8cea35b712b678433fd9cb0329f686e79624
vzina <yeweijian299@163.com>
2020-07-21 21:04:51 +0800

    first commit

---
+++ src/Dispatcher.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Dispatcher.php
+ * PHP version 7
+ *
+ * @category dispatcher
+ * @package  EyPhp\Dispatcher
+ * @author   Weijian.Ye <yeweijian@3k.com>
+ * @license  https://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/vzina
+ */
+declare(strict_types=1);
+
+namespace EyPhp\Dispatcher;
+
+
+class Dispatcher
+{
+
+}
